bd0269a5ce4f15563f7703e8994fd859ceae0dea
AhmedHosni19 <1600432@el-eng.menofia.edu.eg>
2026-02-08 21:52:35 +0200

    Fixing orders history page

--- History.js
+++ History.js
@@ -21,10 +21,9 @@ document.addEventListener("DOMContentLoaded", () => {
             const row = document.createElement("div");
             row.className = "row g-0 ms-3 text-center mb-2";
             row.innerHTML = `
-                <div class="col-3 d-flex align-items-center justify-content-center fw-bold">#ORD-${order.orderId}</div>
-                <div class="col-3 d-flex align-items-center justify-content-center fw-bold">#CUST-${order.userId.slice(-5)}</div>
-                <div class="col-3 d-flex align-items-center justify-content-center fw-bold ">$${order.totalPrice.toFixed(2)}</div>
-                <div class="col-3 d-flex align-items-center justify-content-center fw-bold">${order.status || "Pending"}</div>
+                <div class="col-4 d-flex align-items-center justify-content-center fw-bold">#ORD-${order.orderId}</div>
+                <div class="col-4 d-flex align-items-center justify-content-center fw-bold ">$${order.totalPrice.toFixed(2)}</div>
+                <div class="col-4 d-flex align-items-center justify-content-center fw-bold">${order.status || "Pending"}</div>
                 
             `;
             ordersDiv.appendChild(row);
